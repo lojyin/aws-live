4c2c5a0cd357233ec3c228ad52c0ded769326b8d
lojyin <99168219+lojyin@users.noreply.github.com>
2023-04-13 10:46:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.c8ctnquvqclk.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lojieyin-employee"
+customregion = "us-east-1"
